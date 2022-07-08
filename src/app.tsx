97f69583615ea2496c3f02cd431d9f60806e2598
Yiyang Xu <xuyiyangwing@gmail.com>
2022-07-08 12:31:54 +0800

    chore: add examples

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,25 @@
-import * as React from "react";
 import { BasicUsage } from "./basic-usage";
+import { WithComputed } from "./with-computed";
 import { WithMiddleware } from "./with-middleware";
 import { WithProvider } from "./with-provider";
 import { WithRxJS } from "./with-rxjs";
 import { WithSelector } from "./with-selector";
 import { WithSelectorAuto } from "./with-selector-auto";
 import { WithSetState } from "./with-set-state";
+import { WithTransientScope } from "./with-transient-scope";
 
 export const App = () => {
   return (
     <div>
       <BasicUsage />
+      <WithComputed />
       <WithMiddleware />
       <WithProvider />
       <WithSetState />
       <WithRxJS />
       <WithSelector />
       <WithSelectorAuto />
+      <WithTransientScope />
     </div>
   );
 };
